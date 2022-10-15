4c8e50a32922c773c1b3d3ba18caffc7c08461a0
Nevo Golan <nevog@elementor.com>
2022-10-15 23:20:53 +0300

    wip!

--- src/core/game/moves.ts
+++ src/core/game/moves.ts
@@ -1,6 +1,6 @@
 import { BoardPosition, Piece, PiecesPositionDictionary } from "../../types/game";
 import { makeStringPosition } from "./position";
-import { boardRanks } from "./board";
+import { boardFiles, boardRanks } from "./board";
 
 /**
  * Check if position exists in positions collection.
@@ -12,44 +12,98 @@ export function isPositionInsidePositionsCollection(
   return positions.map(makeStringPosition).includes(makeStringPosition(position));
 }
 
-const calcRankStraightMoves = (piece: Piece, pieces: PiecesPositionDictionary): BoardPosition[] => {
+type MoveCalculationFunction = (
+  [fileIndex, rankIndex]: [number, number],
+  delta: number
+) => BoardPosition;
+
+type MoveCalculationKeys =
+  | "toTop"
+  | "toBottom"
+  | "toLeft"
+  | "toRight"
+  | "toTopLeft"
+  | "toTopRight"
+  | "toBottomLeft"
+  | "toBottomRight";
+
+type MoveCalculation = {
+  [key in MoveCalculationKeys]: MoveCalculationFunction;
+};
+
+const movesCalculation: MoveCalculation = {
+  toTop: ([fileIndex, rankIndex], delta) => {
+    return [boardFiles[fileIndex], boardRanks[rankIndex + delta]];
+  },
+  toBottom: ([fileIndex, rankIndex], delta) => {
+    return [boardFiles[fileIndex], boardRanks[rankIndex - delta]];
+  },
+  toLeft: ([fileIndex, rankIndex], delta) => {
+    return [boardFiles[fileIndex - delta], boardRanks[rankIndex]];
+  },
+  toRight: ([fileIndex, rankIndex], delta) => {
+    return [boardFiles[fileIndex + delta], boardRanks[rankIndex]];
+  },
+  toTopLeft: ([fileIndex, rankIndex], delta) => {
+    return [boardFiles[fileIndex - delta], boardRanks[rankIndex + delta]];
+  },
+  toTopRight: ([fileIndex, rankIndex], delta) => {
+    return [boardFiles[fileIndex + delta], boardRanks[rankIndex + delta]];
+  },
+  toBottomLeft: ([fileIndex, rankIndex], delta) => {
+    return [boardFiles[fileIndex - delta], boardRanks[rankIndex - delta]];
+  },
+  toBottomRight: ([fileIndex, rankIndex], delta) => {
+    return [boardFiles[fileIndex + delta], boardRanks[rankIndex - delta]];
+  },
+};
+
+export function calcQueenMoves(piece: Piece, pieces: PiecesPositionDictionary): BoardPosition[] {
   if (!piece.position) {
     return [];
   }
 
   const [file, rank] = piece.position;
-  const pieceRankIndex = boardRanks.indexOf(rank);
 
-  const beforePiece = boardRanks.slice(0, pieceRankIndex).reverse();
-  const afterPiece = boardRanks.slice(pieceRankIndex + 1);
+  let optionalMoves = [
+    movesCalculation.toTop,
+    movesCalculation.toBottom,
+    movesCalculation.toLeft,
+    movesCalculation.toRight,
+    movesCalculation.toTopLeft,
+    movesCalculation.toTopRight,
+    movesCalculation.toBottomLeft,
+    movesCalculation.toBottomRight,
+  ];
 
-  return [beforePiece, afterPiece].flatMap<BoardPosition>((ranks) => {
-    let blocked = false;
-    return ranks.reduce<BoardPosition[]>((acc, rank) => {
-      if (blocked) {
-        return acc;
+  const fileIndex = boardFiles.indexOf(file);
+  const rankIndex = boardRanks.indexOf(rank);
+
+  const moves: BoardPosition[] = [];
+
+  for (let i = 0; i < boardRanks.length; i++) {
+    for (const moveFn of optionalMoves) {
+      const move = moveFn([fileIndex, rankIndex], i + 1);
+
+      const isValidMove = move[0] && move[1];
+
+      if (!isValidMove) {
+        continue;
       }
 
-      const pieceAtPosition = pieces[makeStringPosition([file, rank])];
+      // const pieceAtPosition = pieces[makeStringPosition(move)];
+      //
+      // if (pieceAtPosition) {
+      //   optionalMoves = optionalMoves.splice(optionalMoves.indexOf(moveFn), 1);
+      //
+      //   if (pieceAtPosition.color === piece.color) {
+      //     continue;
+      //   }
+      // }
 
-      if (pieceAtPosition) {
-        blocked = true;
+      moves.push(move);
+    }
+  }
 
-        if (pieceAtPosition.color === piece.color) {
-          return acc;
-        }
-      }
-
-      acc.push([file, rank]);
-
-      return acc;
-    }, []);
-  });
-};
-
-export const calcStraightMoves = (
-  piece: Piece,
-  pieces: PiecesPositionDictionary
-): BoardPosition[] => {
-  return calcRankStraightMoves(piece, pieces);
-};
+  return moves;
+}
--- src/services/game/selectors.ts
+++ src/services/game/selectors.ts
@@ -1,7 +1,7 @@
 import { RootState } from "../../store";
 import { createSelector } from "reselect";
 import { makeStringPosition } from "../../core/game/position";
-import { calcStraightMoves } from "../../core/game/moves";
+import { calcQueenMoves } from "../../core/game/moves";
 import {
   BoardPosition,
   PiecesIdDictionary,
@@ -35,8 +35,8 @@ export const selectPiecesOptionalMovesDictionary = createSelector(
   [selectPiecesPositionDictionary],
   (pieces) => {
     return Object.values(pieces).reduce<Partial<PiecesAvailableMovesDictionary>>((acc, piece) => {
-      if (["queen", "rook"].includes(piece.type)) {
-        acc[piece.id] = calcStraightMoves(piece, pieces);
+      if (["queen"].includes(piece.type)) {
+        acc[piece.id] = calcQueenMoves(piece, pieces);
       } else {
         acc[piece.id] = [];
       }
